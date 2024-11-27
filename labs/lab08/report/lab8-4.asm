caf0e53736241ac1039576cbe6974abc2bcad31b
iazubov <ivanzub542@gmail.com>
2024-11-27 17:39:58 +0300

    Add files via upload

---
+++ labs/lab08/report/lab8-4.asm
@@ -0,0 +1,32 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg db "Результат: ",0
+
+SECTION .text
+global _start
+
+_start:
+  pop ecx
+  pop edx
+  sub ecx, 1
+  mov esi, 0
+next:
+  cmp ecx, 0h
+  jz _end
+  pop eax
+  call atoi
+
+  mov ebx, eax
+  imul ebx, 12       
+  sub ebx, 7         
+
+  add esi, ebx
+  loop next
+
+_end:
+  mov eax, msg
+  call sprint
+  mov eax, esi
+  call iprintLF
+  call quit
